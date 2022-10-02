a2a05fe010ae6e6558d614d7898227d0c0656662
ELENA <battilena@gmail.com>
2022-10-02 20:23:23 +0300

    method

--- task/Program.cs
+++ task/Program.cs
@@ -6,3 +6,14 @@
 
  string[] array1 = new string[3] {"2", "hello", "world"};
  string[] array2 = new string[array1.Length];
+void FinalArray(string[] array1, string[]array2){
+    int count =0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <=3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
